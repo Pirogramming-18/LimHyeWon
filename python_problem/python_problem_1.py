9b31a2adc7bb890f7ff8ae71ee585ca70bba1dc9
oh-bom <spdlqj888888@gmail.com>
2023-01-06 00:46:20 +0900

    3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,13 @@
 num=0
-select=int(input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):'))
+
+print("시작")
+while 1:
+    try:
+        select=int(input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):'))
+    except:#정수형으로 변환 불가일때
+        print('정수를 입력하세요')
+    else:
+        if (select!= 1 and select !=2 and select !=3):
+            print('1,2,3중 하나를 입력하세요')
+        else:break
+
