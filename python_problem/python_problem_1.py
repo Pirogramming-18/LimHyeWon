06251158d3665914743b6a916fcb0d36d0a80d59
oh-bom <spdlqj888888@gmail.com>
2023-01-06 01:28:57 +0900

    5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,8 +1,7 @@
 num=0
+character=1 #1일땐 A, -1 일땐 B 플레이어
 
 while 1:
-    character=['A','B']
-
     try:
         select=int(input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):'))
     except:#정수형으로 변환 불가일때
@@ -13,6 +12,9 @@ while 1:
         else:
             for i in range(select):
                 num+=1
-                print('player{}:{}'.format(character[0],num))
-            break
+                if character==1:#A player
+                    print('player{}:{}'.format('A', num))
+                else:
+                    print('player{}:{}'.format('B', num))
+            character*=(-1) #플레이어 전환을 위한 값 변경경
 
