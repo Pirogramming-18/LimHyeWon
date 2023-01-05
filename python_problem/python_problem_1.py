fe55dbc520ebd0e2adc09ef5a487945276ca490e
oh-bom <spdlqj888888@gmail.com>
2023-01-06 01:52:06 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,8 +1,9 @@
+import sys
 num=0
-character=['A','B']
 index=0 #character 배열의 인덱스
 
 while 1:
+    character=['A','B']
     try:
         select=int(input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):'))
     except:#정수형으로 변환 불가일때
@@ -13,9 +14,8 @@ while 1:
         else:
             for i in range(select):
                 num+=1
-                if num==31:
-                    print('player{} is win!'.format(character[index]))
                 print('player{}:{}'.format(character[index], num))
+
             if index==0: index=1#플레이어 전환을 위한 값 변경경
             else: index=0
 
