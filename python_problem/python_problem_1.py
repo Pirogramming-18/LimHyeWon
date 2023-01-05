de085b6d7bb5bfddc6ec5758af46b25023ff490a
oh-bom <spdlqj888888@gmail.com>
2023-01-06 01:18:57 +0900

    4단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,6 +1,8 @@
 num=0
 
 while 1:
+    character=['A','B']
+
     try:
         select=int(input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):'))
     except:#정수형으로 변환 불가일때
@@ -8,5 +10,9 @@ while 1:
     else:
         if (select!= 1 and select !=2 and select !=3):
             print('1,2,3중 하나를 입력하세요')
-        else:break
+        else:
+            for i in range(select):
+                num+=1
+                print('player{}:{}'.format(character[0],num))
+            break
 
