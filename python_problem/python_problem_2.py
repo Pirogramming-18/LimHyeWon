04507f3bd060170b9ebd666cad1e19aa28d5d5c2
oh-bom <spdlqj888888@gmail.com>
2023-01-06 15:25:52 +0900

    최종

--- python_problem/python_problem_2.py
+++ python_problem/python_problem_2.py
@@ -1,6 +1,5 @@
 import sys
 student_list=[]
-#함수 이름은 변경 가능합니다.
 
 ##############  menu 1
 def Menu1(name,mid_score,final_score) :
@@ -62,18 +61,17 @@ while True :
         try:
             name,mid_score,final_score=input('Enter name mid-score final-score: ').split(' ')
 
-            # if (name is None) or (mid_score is None) or (final_score is None):
-            #     raise Exception('Num of data is not 3!')
-
+            #이름 겹칠때
             for student in student_list:
                 if student['name']==name:
                     raise Exception('Already exist name!')
 
+            #score에 정수 이외의 것을 입력했을때
             if not (mid_score.isdigit() and final_score.isdigit()):
                 raise Exception('Score is not positive integer!')
 
 
-
+        #data입력을 3개보다 덜 했을때
         except ValueError:
             print('Num of data is not 3!')
 
@@ -84,15 +82,11 @@ while True :
             mid_score = int(mid_score)
             final_score = int(final_score)
             Menu1(name,mid_score,final_score)
-        #예외사항 처리(데이터 입력 갯수, 이미 존재하는 이름, 입력 점수 값이 양의 정수인지)
-        #예외사항이 아닌 입력인 경우 1번 함수 호출
 
     elif choice == "2" :
         try:
             if not student_list: raise Exception
-        #예외사항 처리(저장된 학생 정보의 유무)
-        #예외사항이 아닌 경우 2번 함수 호출
-        #"Grading to all students." 출력
+            
         except:print('No student data')
         else:Menu2(student_list)
 
@@ -102,32 +96,27 @@ while True :
             for student in student_list:
                 if student['grade']=='0':
                     raise Exception("There is a student who didn't get grade")
+                
         except Exception as e:
             print(e)
 
         else:Menu3(student_list)
-        #예외사항 처리(저장된 학생 정보의 유무, 저장되어 있는 학생들의 학점이 모두 부여되어 있는지)
-        #예외사항이 아닌 경우 3번 함수 호출
 
     elif choice == "4" :
         try:
             if not student_list: raise Exception('No student data')
 
             del_student_name=input('Enter the name to delete: ')
-            del_student={}#삭제학생을 담아놓을 리스트
+            del_student={} #삭제학생을 담아놓을 리스트
             for student in student_list:
                 if student['name']==del_student_name:
                     del_student=student
                     Menu4(del_student)
                     print('{} student information is deleted'.format(del_student_name))
-            if not del_student:
+            if not del_student:#삭제 학생에 아무것도 안담겼다면
                 print("Not exist name!")
 
         except Exception as e: print(e)
-        #예외사항 처리(저장된 학생 정보의 유무)
-        #예외사항이 아닌 경우, 삭제할 학생 이름 입력 받기
-        #입력 받은 학생의 존재 유무 체크 후, 없으면 "Not exist name!" 출력
-        #있으면(예를 들어 kim 이라 하면), 4번 함수 호출 후에 "kim student information is deleted." 출력
 
     elif choice == "5" :
         print('exit program!')
@@ -136,4 +125,3 @@ while True :
 
     else :
         print("wrong number. choose again")
-        #"Wrong number. Choose again." 출력
